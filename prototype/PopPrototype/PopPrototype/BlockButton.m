c8df55907ed67a6d13dd733e041b2468751a17f6
Sam Davies <sdavies@shinobicontrols.com>
2014-08-15 21:05:51 +0100

    Added basic animation to the button
    See how simple it is :)

--- prototype/PopPrototype/PopPrototype/BlockButton.m
+++ prototype/PopPrototype/PopPrototype/BlockButton.m
@@ -7,6 +7,7 @@
 //
 
 #import "BlockButton.h"
+#import <POP/POP.h>
 
 IB_DESIGNABLE
 @implementation BlockButton
@@ -36,6 +37,25 @@ IB_DESIGNABLE
 
 - (void)configureButton {
   self.layer.cornerRadius = 5.0;
+  
+  // Add some touch handlers
+  [self addTarget:self action:@selector(zoomIn) forControlEvents:UIControlEventTouchDown];
+  [self addTarget:self action:@selector(zoomDefault) forControlEvents:UIControlEventTouchUpInside];
+}
+
+
+- (void)zoomIn {
+  POPBasicAnimation *zoomAnimation = [POPBasicAnimation animationWithPropertyNamed:kPOPLayerScaleXY];
+  zoomAnimation.toValue = [NSValue valueWithCGSize:CGSizeMake(0.9, 0.9)];
+  [self.layer pop_addAnimation:zoomAnimation forKey:@"layerZoomInAnimation"];
+}
+
+- (void)zoomDefault {
+  POPSpringAnimation *zoomAnimation = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerScaleXY];
+  zoomAnimation.toValue = [NSValue valueWithCGSize:CGSizeMake(1.0, 1.0)];
+  zoomAnimation.velocity = [NSValue valueWithCGSize:CGSizeMake(3.0, 3.0)];
+  zoomAnimation.springBounciness = 25.0;
+  [self.layer pop_addAnimation:zoomAnimation forKey:@"layerZoomDefaultAnimation"];
 }
 
 @end
