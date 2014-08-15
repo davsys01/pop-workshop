f28173255928c042bd5a27e7614ca66a6468787b
Sam Davies <sdavies@shinobicontrols.com>
2014-08-15 20:36:10 +0100

    Reorganise and create objC project
    This is going to be easier than starting out with Swift

---
+++ prototype/PopPrototype/PopPrototype/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PopPrototype
+//
+//  Created by Sam Davies on 15/08/2014.
+//  Copyright (c) 2014 ShinobiControls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
