486fa03e40c52569b0f88db969fb6d53a23b4c11
Sam Davies <sdavies@shinobicontrols.com>
2014-08-22 09:39:16 -0600

    Can even handle rotation now

--- prototype/PopPrototype/PopPrototype/MainViewController.m
+++ prototype/PopPrototype/PopPrototype/MainViewController.m
@@ -22,15 +22,7 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   self.transitioningDelegate = [OverlayTransitioningDelegate new];
-  NSArray *imageNames = @[@"green", @"yellow", @"soft", @"droplet"];
-  NSMutableArray *images = [NSMutableArray new];
-  [imageNames enumerateObjectsUsingBlock:^(NSString *name, NSUInteger idx, BOOL *stop) {
-    [images addObject:[UIImage imageNamed:name]];
-  }];
-  self.zoomableImageCollection = [[ZoomableImageCollection alloc] initWithFrame:self.view.bounds
-                                                                         images:[images copy]
-                                                                    imageHeight:200];
-  [self.view insertSubview:self.zoomableImageCollection atIndex:0];
+  [self createZoomableImageCollection];
 }
 
 
@@ -40,6 +32,29 @@
     destinationVC.modalPresentationStyle = UIModalPresentationCustom;
     destinationVC.transitioningDelegate = self.transitioningDelegate;
   }
+}
+
+
+- (void)createZoomableImageCollection {
+  NSArray *imageNames = @[@"green", @"yellow", @"soft", @"droplet"];
+  NSMutableArray *images = [NSMutableArray new];
+  [imageNames enumerateObjectsUsingBlock:^(NSString *name, NSUInteger idx, BOOL *stop) {
+    [images addObject:[UIImage imageNamed:name]];
+  }];
+  self.zoomableImageCollection = [[ZoomableImageCollection alloc] initWithFrame:self.view.bounds
+                                                                         images:[images copy]
+                                                                    imageHeight:200];
+  
+  [self.view insertSubview:self.zoomableImageCollection atIndex:0];
+  
+  NSDictionary *views = @{@"v": self.zoomableImageCollection};
+  NSArray *vConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"V:|[v]|" options:NSLayoutFormatAlignAllBaseline metrics:nil views:views];
+  NSArray *hConstraints = [NSLayoutConstraint constraintsWithVisualFormat:@"|[v]|" options:NSLayoutFormatAlignAllBaseline metrics:nil views:views];
+  
+  [NSLayoutConstraint activateConstraints:vConstraints];
+  [NSLayoutConstraint activateConstraints:hConstraints];
+  
+  self.zoomableImageCollection.translatesAutoresizingMaskIntoConstraints = NO;
 }
 
 @end
