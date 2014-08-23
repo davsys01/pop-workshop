8b9dff799d46955162949b1c481d2568d90397e1
Sam Davies <sdavies@shinobicontrols.com>
2014-08-23 17:08:44 -0600

    Phase 5: Custom animations on "do not press"
    Create utility methods for shaking and hiding views
    Update the button handler
    Add manual hiding to viewDidLoad

--- PopTastic/PopTastic/PTCWelcomeViewController.m
+++ PopTastic/PopTastic/PTCWelcomeViewController.m
@@ -15,6 +15,8 @@
 //
 
 #import "PTCWelcomeViewController.h"
+#import <POP/POP.h>
+#import <Tweaks/FBTweakInline.h>
 
 @interface PTCWelcomeViewController ()
 
@@ -25,13 +27,57 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   // Do any additional setup after loading the view, typically from a nib.
+  [self animateView:self.doNotPressLabel toHidden:YES];
+  self.doNotPressLabel.hidden = NO;
 }
 
 - (IBAction)handleDoNotPressPressed:(id)sender {
-  self.doNotPressLabel.hidden = NO;
-  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-    self.doNotPressLabel.hidden = YES;
+  dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+    [self shakeView:self.doNotPressButton];
+    [self animateView:self.doNotPressLabel toHidden:NO];
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+      [self animateView:self.doNotPressLabel toHidden:YES];
+    });
   });
+}
+
+#pragma mark - Utility methods
+- (void)shakeView:(UIView *)view {
+  view.userInteractionEnabled = NO;
+  
+  POPSpringAnimation *shakeAnimation = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerPositionX];
+  shakeAnimation.velocity = @(FBTweakValue(@"Do Not Press", @"Button Shake", @"Velocity", 2000, 0, 3000));
+  shakeAnimation.springBounciness = FBTweakValue(@"Do Not Press", @"Button Shake", @"Bounciness", 20, 0, 40);
+  [shakeAnimation setCompletionBlock:^(POPAnimation *animation, BOOL completed) {
+    view.userInteractionEnabled = YES;
+  }];
+  
+  [view.layer pop_addAnimation:shakeAnimation forKey:@"shakeAnimation"];
+}
+
+- (void)animateView:(UIView *)view toHidden:(BOOL)hide {
+  CGFloat opacity = hide ? 0.0 : 1.0;
+  CGFloat scale   = hide ? FBTweakValue(@"Do Not Press", @"Label", @"Hidden Scale", 0.3, 0.0, 3.0) : 1.0;
+  CGFloat yOffset = hide ? FBTweakValue(@"Do Not Press", @"Label", @"Hidden yOffset", -50.0, -100.0, 100.0) : 0.0;
+  
+  POPBasicAnimation *opacityAnimation = [POPBasicAnimation animationWithPropertyNamed:kPOPLayerOpacity];
+  opacityAnimation.toValue = @(opacity);
+  [self.doNotPressLabel.layer pop_addAnimation:opacityAnimation forKey:@"opacityAnimation"];
+  
+  CGFloat springBounciness = FBTweakValue(@"Do Not Press", @"Label", @"Bounciness", 4.0, 0, 50.0);
+  CGFloat springVelocity = FBTweakValue(@"Do Not Press", @"Label", @"Velocity", 20.0, 0, 100.0);
+  
+  POPSpringAnimation *scaleAnimation = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerScaleXY];
+  scaleAnimation.toValue = [NSValue valueWithCGSize:CGSizeMake(scale, scale)];
+  scaleAnimation.velocity = [NSValue valueWithCGSize:CGSizeMake(springVelocity, springVelocity)];
+  scaleAnimation.springBounciness = springBounciness;
+  [self.doNotPressLabel.layer pop_addAnimation:scaleAnimation forKey:@"scaleAnimation"];
+  
+  POPSpringAnimation *yOffsetAnimation = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerTranslationY];
+  yOffsetAnimation.toValue = @(yOffset);
+  yOffsetAnimation.velocity = @(springVelocity);
+  yOffsetAnimation.springBounciness = springBounciness;
+  [self.doNotPressLabel.layer pop_addAnimation:yOffsetAnimation forKey:@"yOffsetAnimation"];
 }
 
 @end
